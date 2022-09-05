dcb25419cf0b8dac1681d42176c29d8696aa49c1
Kalalau Cantrell <cantrellkalalau@gmail.com>
2022-09-04 21:16:56 -0400

    Switch to Jetpack compose in instant app

--- ExploreReactNativeAppClipPassKit/android/instant/build.gradle.kts
+++ ExploreReactNativeAppClipPassKit/android/instant/build.gradle.kts
@@ -10,6 +10,23 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        compose = true
+    }
+
+    // Set both the Java and Kotlin compilers to target Java 8.
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0"
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -28,9 +45,23 @@ dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
     implementation("androidx.appcompat:appcompat:1.5.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // [Compose] Integration with activities
+    implementation("androidx.activity:activity-compose:1.5.1")
+    // [Compose] Material Design
+    implementation("androidx.compose.material:material:1.2.1")
+    // [Compose] Animations
+    implementation("androidx.compose.animation:animation:1.2.1")
+    // [Compose] Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:1.2.1")
+    // [Compose] Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    // [Compose] Update system UI (status bar, action bar, navigation bar)
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.25.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.annotation:annotation:1.4.0")
+    // [Compose] UI Tests
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.1")
 }
--- ExploreReactNativeAppClipPassKit/android/instant/src/main/java/com/explorereactnativeappclippasskit/InstantActivity.kt
+++ ExploreReactNativeAppClipPassKit/android/instant/src/main/java/com/explorereactnativeappclippasskit/InstantActivity.kt
@@ -1,11 +1,138 @@
 package com.explorereactnativeappclippasskit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-class InstantActivity: AppCompatActivity() {
+class InstantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_instant)
+
+        setContentView(
+            ComposeView(this).apply {
+                setContent {
+                    val systemUiController = rememberSystemUiController()
+                    val useDarkIcons = !isSystemInDarkTheme()
+
+                    SideEffect {
+                        systemUiController.setStatusBarColor(Color.Transparent, darkIcons = useDarkIcons)
+                        systemUiController.setNavigationBarColor(LightGray, darkIcons = useDarkIcons)
+                    }
+
+                    MyComposeApplicationTheme {
+                        Greeting()
+                    }
+                }
+            }
+        )
     }
 }
+
+@Composable
+private fun Greeting() {
+    Text(
+        text = stringResource(R.string.hello),
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .wrapContentHeight(Alignment.CenterVertically)
+    )
+}
+
+@Composable
+fun MyComposeApplicationTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
+
+// Color tokens
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val LightGray = Color(0xFFEDEDED)
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+// Shapes
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
+
+// Set of Material typography styles to start with
+
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
